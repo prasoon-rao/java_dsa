7f294c40d9384a46f3e1addc94d05311a98afe1b
prasoon-rao <uttarpradeshsarkar99@gmail.com>
2024-11-21 07:21:10 +0530

    intials

--- pattern1.java
+++ pattern1.java
@@ -2,6 +2,24 @@ public class pattern1
 {
     public static void main(String args[])
     {
-        System.out.println("hello world");
+       int n=5;
+       int i,j;
+       int st=n,sp=0;
+       for (i=1;i<=n;i++)
+       {
+           for (j=1;j<=sp;j++)
+           {
+            System.out.print("\t");
+           }
+           for (j=1;j<=st;j++)
+           {
+            System.out.print("*\t");
+
+           }
+           st--;
+           sp++;
+           System.out.println();
+           
+       }
     }
 }
